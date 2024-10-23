c240e7eb062ac6d94118a485efa596b9193ee0e3
Christopher <christonest@hotmail.com>
2024-10-23 14:13:48 +0200

    Created a utility package and added the MyUtility class in it

--- src/test/java/be/abis/exercise/CourseTest.java
+++ src/test/java/be/abis/exercise/CourseTest.java
@@ -1,6 +1,7 @@
 package be.abis.exercise;
 
 import be.abis.exercise.model.Course;
+import be.abis.exercise.utility.MyUtility;
 
 public class CourseTest {
 
@@ -8,5 +9,7 @@ public class CourseTest {
         Course course = new Course(15, "REST API", "REST API (long name)", 5, 500);
         System.out.println("The " + course.getShortTitle() + " course takes " + course.getNumberOfDays() + " days and costs " + course.getPricePerDay() + " per day.");
         System.out.println(course);
+        course = new Course(39, "Kotlin", "Kotlin (long name)", 10, 870);
+        MyUtility.printCourse(course);
     }
 }
